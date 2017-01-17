107272bdd142cb51a8e305cb359ecf8cda93cf6e
xtayalf <zhangwj0101@gmail.com>
2017-01-17 15:40:00 +0800

    s

--- DataProcess/src/main/java/NMTFUtils/GetFWords.java
+++ DataProcess/src/main/java/NMTFUtils/GetFWords.java
@@ -25,8 +25,9 @@ public class GetFWords {
 
     public static void main(String[] args) throws IOException {
         String base = "EBGB";
-        getTopWordsLSFTL(base);
+//        getTopWordsLSFTL(base);
 //        getTopWordsTriTL(base);
+        getTopWordsDTL(base);
     }
 
     public static void getTopWordsLSFTL(String base) throws IOException {
@@ -71,7 +72,7 @@ public class GetFWords {
         List<String> dictsLists = FileUtils
                 .readLines(new File(dicpath));
         List<String> indexLists = FileUtils
-                .readLines(new File("F:\\matlab_code\\TriTL\\index.csv"));
+                .readLines(new File("F:\\matlab_code\\TriTL\\indext.csv"));
         Map<String, String> wordMaps = new HashMap<>();
 
         for (String line : dictsLists) {
@@ -83,7 +84,7 @@ public class GetFWords {
             }
 
         }
-        PrintStream ps = new PrintStream("G:\\毕业设计论文\\写论文\\journal\\" + base + "\\TRITL-TopWords.txt");
+        PrintStream ps = new PrintStream("G:\\毕业设计论文\\写论文\\journal\\" + base + "\\TRITLt-TopWords.txt");
         for (int li = 0; li < indexLists.size(); li++) {
             String[] split = indexLists.get(li).split(",");
             StringBuilder topWords = new StringBuilder();
@@ -104,4 +105,41 @@ public class GetFWords {
 
     }
 
+    public static void getTopWordsDTL(String base) throws IOException {
+        int TOP_NUM = 20;
+        List<String> dictsLists = FileUtils
+                .readLines(new File(dicpath));
+        List<String> indexLists = FileUtils
+                .readLines(new File("F:\\matlab_code\\DTL\\indexs.csv"));
+        Map<String, String> wordMaps = new HashMap<>();
+
+        for (String line : dictsLists) {
+            String[] split = line.split("@:@");
+            if (split.length != 2) {
+                wordMaps.put(split[0], "");
+            } else {
+                wordMaps.put(split[0], split[1]);
+            }
+
+        }
+        PrintStream ps = new PrintStream("G:\\毕业设计论文\\写论文\\journal\\" + base + "\\DTLs-TopWords.txt");
+        for (int li = 0; li < indexLists.size(); li++) {
+            String[] split = indexLists.get(li).split(",");
+            StringBuilder topWords = new StringBuilder();
+            for (int i = 0; i < TOP_NUM; i++) {
+                String get = wordMaps.get(split[i]);
+                if (get != null) {
+                    topWords.append(get.trim()).append(" ");
+                } else {
+                    System.out.println(split[i]);
+                    return;
+                }
+            }
+            System.out.println(topWords);
+            ps.println("TOP:" + (li + 1) + " " + topWords);
+        }
+        ps.flush();
+        ps.close();
+
+    }
 }
