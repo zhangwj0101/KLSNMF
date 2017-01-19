79fb15523dc576c957432a1b8993979da31484a8
xtayalf <zhangwj0101@gmail.com>
2017-01-19 16:46:15 +0800

    modify

--- KLSNMF/getFwords.m
+++
@@ -1,8 +0,0 @@
-clear all;
-bestFs = xlsread(strcat('Fs.xls'));
-[sA index] = sort(bestFs,'descend');
-csvwrite('indexs.csv',index');
-
-bestFt = xlsread(strcat('Ft.xls'));
-[sA index] = sort(bestFt,'descend');
-csvwrite('indext.csv',index');
